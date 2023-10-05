d147adc1c9c797268a627a73c368ae99963f1767
szabo_andras <andreas.szabo23@gmail.com>
2023-10-05 07:25:07 +0200

    1st commit

---
+++ src/main/java/com/example/intuitech_hazi/IntuitechHaziApplication.java
@@ -0,0 +1,13 @@
+package com.example.intuitech_hazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuitechHaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntuitechHaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intuitech_hazi/IntuitechHaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intuitech_hazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntuitechHaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
