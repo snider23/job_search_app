e5e11fccfb5d5cd9013356ee8b04224440795da3
szabo_andras <andreas.szabo23@gmail.com>
2023-10-08 20:55:48 +0200

    we can search in our DB or with Reed

--- src/main/java/com/example/intuitech_hazi/repository/PositionRepository.java
+++ src/main/java/com/example/intuitech_hazi/repository/PositionRepository.java
@@ -16,6 +16,6 @@ public interface PositionRepository extends JpaRepository<Position,Long> {
 
     //todo KÉRDÉS: érdmes modositani, mert most ha egy szóban van egy betű akkor azzal a betűvel az összes JobTitle-t megjeleniti
 
-    @Query("SELECT p FROM Position p WHERE p.title LIKE %:jobTitle% OR p.location LIKE %:jobLocation%")
+    @Query("SELECT p FROM Position p WHERE CONCAT(' ', p.title, ' ') LIKE %:jobTitle% AND CONCAT(' ', p.location, ' ') LIKE %:jobLocation%")
     List<Position> findPositionsBy(@Param("jobTitle") String jobTitle, @Param("jobLocation") String jobLocation);
 }
