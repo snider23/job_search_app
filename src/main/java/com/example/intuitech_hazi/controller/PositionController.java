eb97848c25c19ccb5a0280521351174bce843b30
szabo_andras <andreas.szabo23@gmail.com>
2023-10-09 20:56:02 +0200

    clear out unused code

--- src/main/java/com/example/intuitech_hazi/controller/PositionController.java
+++ src/main/java/com/example/intuitech_hazi/controller/PositionController.java
@@ -19,7 +19,6 @@ import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -48,71 +47,27 @@ public class PositionController {
         this.positionService = positionService;
         this.clientService = clientService;
         this.webClient = webClientBuilder
-                        .baseUrl(websiteUrl)
-                        .build();
+                .baseUrl(websiteUrl)
+                .build();
         this.reedJob = reedJob;
         this.externalApiKey = reedJob.getExternalApiKey();
     }
 
 
-@GetMapping("/search")
-public Mono<Object> getAllJobs2(@RequestBody Position position, @RequestHeader("apiKey") String apiKey) {
-    if (!clientService.isApiKeyExists(apiKey)) {
-        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid API key"));
-    }
-
-    return reedJob.getJobsFromExternalApi(position, apiKey)
-            .flatMap(jobs -> {
-                List<PositionListItem> allPositionFromBothDB = new ArrayList<>();
-                allPositionFromBothDB.addAll(jobs);
-                allPositionFromBothDB.addAll(positionService.getComplexPositions(position, apiKey));
-                return Mono.just(allPositionFromBothDB);
-            });
-}
-
-    @GetMapping("/jobs")
-    public Mono<Object> getJobs(@RequestBody Position position, @RequestHeader("apiKey") String apiKey) {
+    @GetMapping("/search")
+    public Mono<Object> getAllJobs2(@RequestBody Position position, @RequestHeader("apiKey") String apiKey) {
         if (!clientService.isApiKeyExists(apiKey)) {
             return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid API key"));
         }
-        List<PositionListItem> allPositionFromBothDB = new ArrayList<>();
 
-
-        return this.webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/api/1.0/search")
-                        .queryParam("keywords", position.getTitle())
-                        .queryParam("locationName", position.getLocation())
-                        .queryParam("resultsToTake", "5")
-                        .build())
-                .accept(MediaType.APPLICATION_JSON)
-                .headers(headers -> headers.setBasicAuth(externalApiKey, ""))
-                .retrieve()
-                .bodyToMono(String.class)
-                .flatMap(json -> {
-                    ObjectMapper mapper = new ObjectMapper();
-                    try {
-                        JsonNode rootNode = mapper.readTree(json);
-                        JsonNode resultsNode = rootNode.get("results");
-                        List<PositionListItem> jobs = new ArrayList<>();
-                        for (JsonNode resultNode : resultsNode) {
-                            PositionListItem job = new PositionListItem();
-                            job.setJobTitle(resultNode.get("jobTitle").asText());
-                            job.setLocationName(resultNode.get("locationName").asText());
-                            job.setJobUrl(resultNode.get("jobUrl").asText());
-                            jobs.add(job);
-                        }
-                        allPositionFromBothDB.addAll(jobs);
-                        allPositionFromBothDB.addAll(positionService.getComplexPositions(position, apiKey));
-                        return Mono.just(allPositionFromBothDB);
-                    } catch (JsonProcessingException e) {
-                        e.printStackTrace();
-                        return Mono.just(Collections.emptyList());
-                    }
-
+        return reedJob.getJobsFromExternalApi(position, apiKey)
+                .flatMap(jobs -> {
+                    List<PositionListItem> allPositionFromBothDB = new ArrayList<>();
+                    allPositionFromBothDB.addAll(jobs);
+                    allPositionFromBothDB.addAll(positionService.getComplexPositions(position, apiKey));
+                    return Mono.just(allPositionFromBothDB);
                 });
     }
-
 
     @PostMapping
     public ResponseEntity<String> createNewJob(@RequestBody Position newPosition,
